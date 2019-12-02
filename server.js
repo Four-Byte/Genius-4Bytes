09d16d9aaa2bce9acdaf59ad149ea1ba7512c790
obada <obadaquraan@yahoo.com>
2019-12-02 11:25:46 +0200

    fix

--- server.js
+++ server.js
@@ -62,16 +62,18 @@ app.post('/dosearches',(request,response)=>{
   console.log('sort : ', sort);
   ;
   let url;
-  let d = new Date();
-  let year=d.getFullYear()
- if(sort === 'popularity') {url=`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.MOV_API}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`}
- if(sort === 'revenue'){url=`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.MOV_API}&language=en-US&sort_by=revenue.desc.desc&include_adult=false&include_video=false&page=1`}
-if(releaseYear){url+=`&primary_release_year=${releaseYear}`}
+  
+ if(sort === 'popularity') {url=`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.MOV_API}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`};
+ if(sort === 'revenue'){url=`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.MOV_API}&language=en-US&sort_by=revenue.desc.desc&include_adult=false&include_video=false&page=1`};
+if(releaseYear){url+=`&primary_release_year=${releaseYear}`};
+console.log('releaseYear : ', releaseYear);
 console.log('url : ', url);
- superagent(url)
+  superagent(url)
  .then(data=>{
-  let movie=data.body.results;
+   let movie=data.body.results;
+   console.log('movie : ', movie);
   let movi= movie.map(val=>{
+    console.log('val : ', val);
    return new Movie(val);
   })
   // console.log('movie : ', movie);
