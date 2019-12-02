63dd8d6b346914d45b28963d5158fb73061ad08b
obada <obadaquraan@yahoo.com>
2019-12-02 10:26:20 +0200

    update and delete

--- schema.sql
+++ schema.sql
@@ -2,12 +2,13 @@ DROP TABLE IF EXISTS movies;
 
 CREATE TABLE movies (
     id serial PRIMARY KEY ,
-    title VARCHAR(256),
-    authors VARCHAR(256),
-    isbn VARCHAR(256),
-    url VARCHAR(256),
-    description text
+    title text,
+    poster_path text,
+    overview text,
+    popularity text,
+    vote_average text,
+    release_date text
 
 );
 
-INSERT INTO movies (title,authors,isbn,url,description) VALUES('foo','ghaid','321541','ghaid.zahran@hotmail.com','foolish book')
+INSERT INTO movies (title,poster_path,overview,popularity,vote_average,release_date) VALUES('movie','dd','321541','22.221','36.33','2020-2-21')
